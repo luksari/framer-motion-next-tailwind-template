fe52b2075f665d00c96303eb215d9945be07e35e
Łukasz Tyszkiewicz <lukasz.tyszkiewicz.contact@gmail.com>
2023-04-13 10:43:49 +0000

    Update 3 files

--- src/pages/drawer.tsx
+++ src/pages/drawer.tsx
@@ -1,6 +1,4 @@
-import Image from 'next/image';
-
-export default function Home() {
+export default function DrawerPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -16,20 +14,14 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="./drawer"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={'mb-3 text-2xl font-semibold'}>
-            Drawer example{' '}
+        <button className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          <h2 className={`text-2xl font-semibold`}>
+            Open drawer{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Example of drawer that is being opened by clicking button. Mount/Unmount animation with AnimatePresence
-          </p>
-        </a>
+        </button>
       </div>
     </main>
   );
